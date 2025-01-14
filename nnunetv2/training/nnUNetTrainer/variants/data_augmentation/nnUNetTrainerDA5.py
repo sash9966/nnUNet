74f9c542d847236f954b38249ce6bdedce246e63
sash9966 <sascha.stocker@hotmail.ch>
2025-01-14 08:35:32 -0800

    trainer DA5 with 100 epoch added.. currently not being found by module

--- nnunetv2/training/nnUNetTrainer/variants/data_augmentation/nnUNetTrainerDA5.py
+++ nnunetv2/training/nnUNetTrainer/variants/data_augmentation/nnUNetTrainerDA5.py
@@ -852,3 +852,9 @@ class nnUNetTrainerDA5_10epochs(nnUNetTrainerDA5):
                  device: torch.device = torch.device('cuda')):
         super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
         self.num_epochs = 10
+        
+class nnUNetTrainerDA5_100epochs(nnUNetTrainerDA5):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = 100
